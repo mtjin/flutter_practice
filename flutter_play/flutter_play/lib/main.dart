3dede37729f404d490f1512e9eaf6de0cf715586
jackjackE <tmddjs210@naver.com>
2021-01-15 21:28:23 +0900

    카운터앱 완성

--- flutter_play/flutter_play/lib/main.dart
+++ flutter_play/flutter_play/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  //바꿀수 없는 위젯
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  //바뀔수 있는 위젯
   final String title;
 
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -23,22 +25,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("버튼을 눌러보세요!!"),
-            Text("Counter", style: Theme
-                .of(context)
-                .textTheme
-                .display1,)
-          ],)
-    )
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("버튼을 눌러보세요!!"),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.display1,
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: '증가',
+        child: Icon(Icons.add),
+      ),
     );
+  }
+
+  void _incrementCounter() {
+    //카운터 증가
+    setState(() {
+      _counter++;
+    });
   }
 }
